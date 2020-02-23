7e5c019da840c31d7e06a2080561e7c2fa629aa0
Ehis <ehisekore@outlook.com>
2020-02-23 17:25:31 -0500

    completed tutorial code

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,52 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing a list of landmarks.
+*/
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject private var userData: UserData
+    
+    
+    /*
+     something was bugging with this body.
+     Faves weren't being updated.
+        I literally typed the code from the final version here myself: no change
+     I gave up and copy/pasted this body
+     */
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Faves Only")
+                }
+                
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(
+                            destination: LandmarkDetail(landmark: landmark)
+                                .environmentObject(self.userData)
+                        ) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarksList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        .environmentObject(UserData())
+    }
+}
